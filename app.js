706172ff95ca30d0210085cf012d2c0c0344889e
rileyparadox <153963448+rileyparadox@users.noreply.github.com>
2023-12-30 22:15:34 +0000

    tmp api request

--- app.js
+++ app.js
@@ -6,4 +6,27 @@ var axios = require('axios'),
 
 require('dotenv').config();
 
+var api_prefix = '/api/v1/';
+
+/* 
+
+function sendTMPApiRequest is used to send requests to the TruckersMP API
+
+*/
+function sendTMPApiRequest(url, type = "get") {
+  
+  switch (type) {
+    case "get":
+      return axios.get(url);
+      break;
+    case "post":
+      return axios.post(url);
+      break;
+    default:
+      return axios.get(url);
+      break;
+  }
+}
+
+
 var steam = new steamapi(process.env.STEAM_API_KEY);
